77a692e94510e2752ba5fc92ac0f894ae5bd0df5
Ross Caisse <rcaisse@smashfly.com>
2019-02-10 20:42:13 -0500

    Adding basic line scraping and updating README with windows Tidy instructions

--- README.md
+++ README.md
@@ -12,5 +12,10 @@ Used for parsing HTML
 
 Used to correct issues with HTML before parsing
 
+#### Ubuntu
     apt-get install libtidy
     pip install pytidylib
+
+#### Windows
+
+Download the binaries from [here](http://binaries.html-tidy.org/). Unzip and add directory with tidy.dll to system PATH variable.
--- scrapers/espn_scraper.py
+++ scrapers/espn_scraper.py
@@ -1,9 +1,12 @@
 import sys
 import os
+import re
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
 from sa_common.models.baseteam import BaseTeam
 from bs4 import BeautifulSoup
 from tidylib import tidy_document
+
+TIME_REGEX = '^([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$'
 
 def scrape_lines(path, isVerbose = False):
     global verbose
@@ -14,18 +17,8 @@ def scrape_lines(path, isVerbose = False):
         doc = testFile.read()
     #doc = urllib2.urlopen(path)
 
-    options = {
-        str("quiet"): True,
-        str("show-errors"): 0,
-        str("force-output"): True,
-        str("numeric-entities"): True,
-        str("show-warnings"): False,
-        str("indent"): False,
-        str("tidy-mark"): False,
-        str("wrap"): 0
-        };
-
-    document, errors = tidy_document(doc, options=options)
+    # TODO Log errors?
+    document, errors = tidy_document(doc)
 
     parsed_data = BeautifulSoup(document, 'html.parser')
 
@@ -41,13 +34,41 @@ def scrape_lines(path, isVerbose = False):
         if "class" in child.attrs:
             # Start of new game
             if child.attrs["class"][0] == "stathead":
-                print("Starting new game")
+                awayTeamString, homeTeamString = scrape_teams(child)
+                if(verbose):
+                    print("Starting new game")
+                    print(f"{awayTeamString} at {homeTeamString}")
+
             # Line data from for one book
             elif child.attrs["class"][0] == "oddrow" or child.attrs["class"][0] == "evenrow":
-                print("Book data")
-                scrape_book_data(child)
+                bookName, spread, overUnder = scrape_book_data(child)
+                if(spread is None or overUnder is None):
+                    print(f"No data for {bookName}")
+                elif(verbose):
+                    print(f"{bookName}: Spread: {spread} O/U: {overUnder}")
 
 
+# Scrape team names
+def scrape_teams(matchup_tag):
+    split_string = [x.strip(',') for x in matchup_tag.td.text.split()]
+    # List of strings is of the form (for example):
+    # [0] North
+    # [1] Carolian
+    # [2] at
+    # [3] Duke
+    # [4] 5:30
+    # [5] PM
+    # So find the index of 'at' and the index of the game time to extract teams
+    atIndex = split_string.index('at')
+    gametimeIndex = [i for i, item in enumerate(split_string) if re.search(TIME_REGEX, item)][0]
+    awayTeamName = " ".join([e for i, e in enumerate(split_string) if i in [0, atIndex - 1]])
+    homeTeamName = " ".join([e for i, e in enumerate(split_string) if i in [atIndex + 1, gametimeIndex - 1]])
+
+    # TODO remove rankings ('#22')
+
+    return awayTeamName, homeTeamName
+
+# Scrape line and over/under data for an individual book
 # <tr class="oddrow">
 #     <td>Westgate</td>
 #     <td style="text-align:center;">
@@ -70,14 +91,28 @@ def scrape_lines(path, isVerbose = False):
 #     </td>
 # </tr>
 def scrape_book_data(book_tag):
-
     firstRow = book_tag.td
-    print(firstRow)
+    bookName = firstRow.text.strip().replace('\r','').replace('\n','').replace('  ', '')
+    spread = None
+    overUnder = None
+    retrievedSpread = False
     for child in firstRow.next_siblings:
-        if child is Tag:
-            print(child)
+        if child == '\n':
+            continue
+        elif retrievedSpread == False:
+            try:
+                spread = float(child.find("td").text.split()[0])
+            except:
+                spread = None
+            retrievedSpread = True
         else:
-            print("Nope")
+            try:
+                overUnder = float(child.find("td").text.strip())
+            except:
+                overUnder = None
+            break
+
+    return bookName,spread,overUnder
 
     #data = book_tag.find_all("td")
     #if len(data) != 4:
