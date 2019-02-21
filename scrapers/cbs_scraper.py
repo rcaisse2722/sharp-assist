9e681f5b84087e73b5611cb699dbf06f22fe56c0
Ross Caisse <rcaisse@smashfly.com>
2019-02-20 22:26:35 -0500

    Adding basic score scraping

--- scrapers/cbs_scraper.py
+++ scrapers/cbs_scraper.py
@@ -18,5 +18,34 @@ def scrape_scores(data_provider, team_repository, isVerbose = False):
     games = parsed_data.find_all("div", {"class" : "in-progress-table"}) 
     
     for game in games:
-        away_team = " ".join(game.find("a", {"class" : "team"}).text.strip().split())
-        print(away_team)
+        teams = game.find_all("a", {"class" : "team"})
+        
+        try:
+            away_team_str = " ".join(teams[0].text.strip().split())
+            home_team_str = " ".join(teams[1].text.strip().split())
+        except:
+            print("Error parsing score from CBS")
+            continue
+
+        home_team = team_repository.get_team(home_team_str)
+        if(home_team == None):
+            print(f"Team {home_team_str} not found in repository")
+            break
+        away_team = team_repository.get_team(away_team_str)
+        if(away_team == None):
+            print(f"Team {away_team_str} not found in repository")
+            break
+        away_team_score = extract_score(teams[0].parent)
+        home_team_score = extract_score(teams[1].parent)
+
+        print(f"{away_team_str} at {home_team_str}: {away_team_score} - {home_team_score}")
+        
+
+def extract_score(parent_tag):
+    lastTag = None
+    for child in parent_tag.next_siblings:
+        if child == '\n':
+            continue
+        elif child.name == 'td':
+            lastTag = child
+    return int(lastTag.text)
